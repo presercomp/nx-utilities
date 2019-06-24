8a2f407988f2bff38a94d2ae92e97c80cb8e6988
Sebastián Solar <sebastian.solar@presercomp.cl>
2019-06-23 23:29:31 -0400

    v0.0.2

---
+++ environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+    production: true,    
+    apiURL: '',    
+    segEsperaInactivo: 300, /* 900 segundos = 15 minutos */
+    segEsperaBloqueo: 30,
+    encryptSecretKey: 'Th3Gh0$t1nTh3$heLl',
+    dtOptions : {
+      autoWidth: true,
+      responsive: true,
+      pagingType: 'simple',
+      pageLength: 5,
+      info: true,
+      scrollCollapse: true,
+      search: false,
+      language: {
+        url: 'assets/lang/dataTables.espanol.lang'
+      }
+    }
+  };
